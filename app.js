9887bd4e9bd81606f9caab8b0751b68037b20bf8
ZubairAAC <70068805@student.uol.edu.pk>
2021-10-20 19:15:59 +0500

    with info api

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use('/user', require('./api/controllers/user'));
+app.use('/info', require('./api/controllers/info'));
 app.listen(port, () => console.log('Server started on port 8000'));
 
 
