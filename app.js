6dae37d08a14d5a7e89bf3da3e5618b091085f98
ZubairAAC <70068805@student.uol.edu.pk>
2021-10-21 16:51:42 +0500

    with new

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 let port = process.env.PORT || 8000;
 
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
